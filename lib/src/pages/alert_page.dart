4f5e0e2658ce9622ec5dec5a54deec8c9e79767f
JCamiloo <jcamilo.osorio15@gmail.com>
2020-03-28 11:26:40 -0500

    rounded alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -6,6 +6,45 @@ class AlertPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Alert Page')),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Show alert'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => showAlert(context),
+        ),
+      ),
     );
+  }
+
+  void showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Alert content'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      }
+    ); 
   }
 }
