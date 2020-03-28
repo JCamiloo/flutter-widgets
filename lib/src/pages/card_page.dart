f9c2dc59bfdbe45b6463df6ff24f4eee806cf604
JCamiloo <jcamilo.osorio15@gmail.com>
2020-03-27 23:44:11 -0500

    starting card page

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  const CardPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Card Page')),
+        body: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: <Widget>[_cardClass1()],
+        ),
+      ),
+    );
+  }
+
+  Widget _cardClass1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_size_select_actual, color: Colors.blue),
+            title: Text('Card title'),
+            subtitle: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s')
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ]
+      ),
+    );
+  }
+}
