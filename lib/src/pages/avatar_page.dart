7463084561a61ea7ebf48056f2f59def4e27a69f
JCamiloo <jcamilo.osorio15@gmail.com>
2020-03-28 14:41:31 -0500

    avatar page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -5,7 +5,32 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar Page')),
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JO', style: TextStyle(color: Colors.white)),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://images-na.ssl-images-amazon.com/images/I/A1WB83VWSFL.png'),
+          placeholder: AssetImage('assets/loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
